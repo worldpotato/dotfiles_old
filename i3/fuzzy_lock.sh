ddd52e344f64a742c1daa5748237924be7e4113d
Worldpotato <felix.strobel.fs@gmail.com>
2019-03-04 21:13:33 +0100

    [i3] reduced turn-off time after lock

--- i3/fuzzy_lock.sh
+++ i3/fuzzy_lock.sh
@@ -13,4 +13,4 @@ convert -scale 10% -scale 1000% /tmp/screen_locked.png /tmp/screen_locked.png
 i3lock -i /tmp/screen_locked.png
 
 # Turn the screen off after a delay.
-sleep 60; pgrep i3lock && xset dpms force off
+sleep 10 pgrep i3lock && xset dpms force off

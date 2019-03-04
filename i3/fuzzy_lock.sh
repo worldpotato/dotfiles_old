7e4011956733e703e48d0a58e159487535ea00ca
Worldpotato <felix.strobel.fs@gmail.com>
2019-03-04 21:12:18 +0100

    [i3] lock change from blur to pixel
    Because bluring the image took some time.

--- i3/fuzzy_lock.sh
+++ i3/fuzzy_lock.sh
@@ -3,9 +3,10 @@
 # Take a screenshot
 scrot /tmp/screen_locked.png
 
-# Pixellate it 10x
-#mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-convert -blur 0x10 /tmp/screen_locked.png /tmp/screen_locked.png
+convert -scale 10% -scale 1000% /tmp/screen_locked.png /tmp/screen_locked.png
+
+# blur image
+#convert -blur 0x05 /tmp/screen_locked.png /tmp/screen_locked.png
 #rm /tmp/locking_screen.png
 
 # Lock screen displaying this image.

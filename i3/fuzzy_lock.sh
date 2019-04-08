cc58d2be887d331c227f6df1a81bbc9fbdea546e
Worldpotato <worldpotato@Virus.sh>
2019-04-08 16:04:42 +0200

    [i3lock] use a fix image

--- i3/fuzzy_lock.sh
+++ i3/fuzzy_lock.sh
@@ -1,16 +1,5 @@
 #!/bin/sh -e
-
-# Take a screenshot
-scrot /tmp/screen_locked.png
-
-convert -scale 10% -scale 1000% /tmp/screen_locked.png /tmp/screen_locked.png
-
-# blur image
-#convert -blur 0x05 /tmp/screen_locked.png /tmp/screen_locked.png
-#rm /tmp/locking_screen.png
-
-# Lock screen displaying this image.
-i3lock -i /tmp/screen_locked.png
+i3lock -i /home/worldpotato/.config/i3/overTheClouds.png
 
 # Turn the screen off after a delay.
 sleep 10 pgrep i3lock && xset dpms force off

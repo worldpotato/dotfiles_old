427dfe563bc82848623a0b1ec0b5eea7e60f4ba7
Felix Strobel <mail@felixstrobel.de>
2019-08-22 14:26:24 +0200

    [i3] status 22.8.2019

--- i3/fuzzy_lock.sh
+++ i3/fuzzy_lock.sh
@@ -2,4 +2,5 @@
 i3lock -i /home/worldpotato/.config/i3/overTheClouds.png
 
 # Turn the screen off after a delay.
-sleep 10 pgrep i3lock && xset dpms force off
+sleep 5 
+grep i3lock && xset dpms force off

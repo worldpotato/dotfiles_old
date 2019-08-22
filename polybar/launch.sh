3bc0c2e07c1ffb82b38aed23a360cc2e19f1497a
Felix Strobel <mail@felixstrobel.de>
2019-08-22 14:33:27 +0200

    [polybar] status 22.8.19

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,5 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar1 &
+  done
+else
+  polybar --reload bar1 &
+fi
+
 # Launch bar1 and bar2
-polybar bar1 &
+#polybar bar1 &

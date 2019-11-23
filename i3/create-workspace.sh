7fd197f3e7938d916d89cd3582120719f02f0157
Felix Strobel <mail@felixstrobel.de>
2019-11-23 17:26:30 +0100

    [i3] using termite instead of urxvt at startup

--- i3/create-workspace.sh
+++ i3/create-workspace.sh
@@ -2,6 +2,6 @@
 
 i3-msg "workspace zsh; append_layout ~/repos/dotfiles/i3/zsh_workspace.json"
 
-(urxvt -title first -cd /home/worldpotato &)
-(urxvt -e "gotop" &)
-(urxvt -title clock -e tty-clock -sbcDC 4 -f %d.%m.%Y &)
+(termite --title=first -d /home/worldpotato &)
+(termite -e "gotop -r 0.5" &)
+(termite --title=clock -e "tty-clock -sbcDC 4 -f %d.%m.%Y" &)

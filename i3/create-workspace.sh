3725976b62615788a830b819c802d2f5334e0075
Felix Strobel <mail@felixstrobel.de>
2019-12-02 08:18:32 +0100

    [i3] use alacritty as terminal

--- i3/create-workspace.sh
+++ i3/create-workspace.sh
@@ -2,6 +2,6 @@
 
 i3-msg "workspace zsh; append_layout ~/repos/dotfiles/i3/zsh_workspace.json"
 
-(termite --title=first -d /home/worldpotato &)
-(termite -e "gotop -r 0.5" &)
-(termite --title=clock -e "tty-clock -sbcDC 4 -f %d.%m.%Y" &)
+alacritty --title=first --working-directory /home/worldpotato/ &
+alacritty --title=gotop -e /usr/bin/gotop &
+alacritty --title=clock -e /usr/bin/tty-clock -sbcDC 4 -f %d.%m.%Y &

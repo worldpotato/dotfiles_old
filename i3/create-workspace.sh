c71c69b8519d95fb00ba19bc60edaeab6a3fba59
Felix Strobel <mail@felixstrobel.de>
2019-12-28 10:59:37 +0100

    [i3] restart i3 after creating workspace
    this is neccessary, otherwise alacritty creates shadows

--- i3/create-workspace.sh
+++ i3/create-workspace.sh
@@ -5,3 +5,5 @@ i3-msg "workspace zsh; append_layout ~/repos/dotfiles/i3/zsh_workspace.json"
 alacritty --title=first --working-directory /home/worldpotato/ &
 alacritty --title=gotop -e /usr/bin/gotop &
 alacritty --title=clock -e /usr/bin/tty-clock -sbcDC 4 -f %d.%m.%Y &
+
+i3-msg restart

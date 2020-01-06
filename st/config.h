6e0393cf7e7dc33aa2ae03e2ef4f37c9cf9f6b3b
Felix Strobel <mail@felixstrobel.de>
2020-01-07 00:40:20 +0100

    [st] enhancement for st config

--- st/config.h
+++ st/config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=15:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -178,8 +178,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_j,           kscrollup,      {.i = -1} },
+	{ ControlMask,          XK_k,           kscrolldown,    {.i = -1} },
     { TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
 

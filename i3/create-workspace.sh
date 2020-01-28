56c789fa164fc2d307b137e1436973f659975dc9
Felix Strobel <mail@felixstrobel.de>
2020-01-28 21:44:56 +0100

    [i3] change color of ttyclock on startup

--- i3/create-workspace.sh
+++ i3/create-workspace.sh
@@ -4,6 +4,6 @@ i3-msg "workspace zsh; append_layout ~/repos/dotfiles/i3/workspace-clean.json"
 
 termite --title=first --directory=/home/worldpotato/ &
 termite --title=gotop -e /usr/bin/gotop &
-termite --title=clock -e "/usr/bin/tty-clock -sbcDC 6 -f %d.%m.%Y" &
+termite --title=clock -e "/usr/bin/tty-clock -sbcDC2 -f %d.%m.%Y" &
 
 i3-msg restart
